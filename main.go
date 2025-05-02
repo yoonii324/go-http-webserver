2250a42502bd1418356e078faa720db63c805b03
yoonii324 <jyoonii324@gmail.com>
2025-05-02 01:59:07 -0400

    included logic to make use of query parameters included in http request

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // register web handler
-		fmt.Fprint(w, "Hello World")
-	})
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()    // bring query parameters
+	name := values.Get("name") // check if there are any specific key values
+	if name == "" {
+		name = "World"
+	}
 
-	http.ListenAndServe(":3000", nil) // start a web server
+	id, _ := strconv.Atoi(values.Get("id")) // get the id value and cast it to an integer type
+	fmt.Fprintf(w, "Hello %s! id: %d", name, id)
+}
+
+func main() {
+	http.HandleFunc("/bar", barHandler) // register bar handler
+	http.ListenAndServe(":3000", nil)   // start a web server
 }
