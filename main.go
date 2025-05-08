eaf880e8a6c0dbc8d65565d7215fc510f1ab6c7a
yoonii324 <jyoonii324@gmail.com>
2025-05-08 01:12:46 -0400

    web server that returns data in JSON format

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -29,15 +30,24 @@ import (
 // 	http.ListenAndServe(":3000", mux) // start mux instance
 // }
 
-func MakeWebHandler() http.Handler {
+type Student struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+func MakeWebHandler() http.Handler { // function that makes handler instance
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
-	mux.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello Bar")
-	})
+	mux.HandleFunc("/student", StudentHandler)
 	return mux
+}
+
+func StudentHandler(w http.ResponseWriter, r *http.Request) {
+	var student = Student{"aaa", 16, 87}
+	data, _ := json.Marshal(student)                   // translate Student object into []byte format
+	w.Header().Add("content-type", "application/json") // denote that it is JSON format
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data)) // send the result
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,30 +9,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIndexHandler(t *testing.T) { // path / test
+// func TestIndexHandler(t *testing.T) { // path / test
+// 	assert := assert.New(t)
+
+// 	res := httptest.NewRecorder()
+// 	req := httptest.NewRequest("GET", "/", nil)
+
+// 	mux := MakeWebHandler()
+// 	mux.ServeHTTP(res, req)
+
+// 	assert.Equal(http.StatusOK, res.Code) // check status code
+// 	data, _ := io.ReadAll(res.Body)       // by reading the data
+// 	assert.Equal("Hello World", string(data))
+// }
+
+// func TestBarHandler(t *testing.T) {
+// 	assert := assert.New(t)
+
+// 	res := httptest.NewRecorder()
+// 	req := httptest.NewRequest("GET", "/bar", nil) // path /bar test
+
+// 	mux := MakeWebHandler()
+// 	mux.ServeHTTP(res, req)
+
+// 	assert.Equal(http.StatusOK, res.Code)
+// 	data, _ := io.ReadAll(res.Body)
+// 	assert.Equal("Hello Bar", string(data))
+// }
+
+func TestJsonHandler(t *testing.T) {
 	assert := assert.New(t)
 
 	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/", nil)
+	req := httptest.NewRequest("GET", "/student", nil) // path /student test
 
 	mux := MakeWebHandler()
 	mux.ServeHTTP(res, req)
 
 	assert.Equal(http.StatusOK, res.Code) // check status code
-	data, _ := io.ReadAll(res.Body)       // by reading the data
-	assert.Equal("Hello World", string(data))
-}
-
-func TestBarHandler(t *testing.T) {
-	assert := assert.New(t)
-
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/bar", nil) // path /bar test
-
-	mux := MakeWebHandler()
-	mux.ServeHTTP(res, req)
-
-	assert.Equal(http.StatusOK, res.Code)
-	data, _ := io.ReadAll(res.Body)
-	assert.Equal("Hello Bar", string(data))
+	student := new(Student)
+	err := json.NewDecoder(res.Body).Decode(student) // translate the result
+	assert.Nil(err)                                  // check the result
+	assert.Equal("aaa", student.Name)
+	assert.Equal(16, student.Age)
+	assert.Equal(87, student.Score)
 }
